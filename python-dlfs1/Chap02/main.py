ed4a0247a0b0a4216aebf8f40e0843ea69de51cf
Kang Junhyeong <kangjj17@naver.com>
2026-01-05 20:42:23 +0900

    feat: 다층 퍼셉트론으로 XOR 구현

--- python-dlfs1/Chap02/main.py
+++ python-dlfs1/Chap02/main.py
@@ -1,12 +1,48 @@
+import numpy as np
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
-        return 0
-    elif tmp > theta:
-        return 1
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
     
-print(AND(0, 0))
-print(AND(1, 0))
-print(AND(0, 1))
-print(AND(1, 1))
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+def func(x1, x2):
+    return XOR(x1, x2)
+
+print(func(0, 0))
+print(func(1, 0))
+print(func(0, 1))
+print(func(1, 1))
