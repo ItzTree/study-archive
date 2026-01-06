b4096d05381e4feebb4d0fba55cd221eb7089c2b
Kang Junhyeong <kangjj17@naver.com>
2026-01-06 21:36:15 +0900

    feat: MNIST 신경망 추론 수행

--- python-dlfs1/Chap03/main.py
+++ python-dlfs1/Chap03/main.py
@@ -1,43 +1,7 @@
 import numpy as np
-
-def AND(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.7
-    
-    tmp = np.sum(w*x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
-
-def NAND(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([-0.5, -0.5])
-    b = 0.7
-
-    tmp = np.sum(w*x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
-
-def OR(x1, x2):
-    x = np.array([x1, x2])
-    w = np.array([0.5, 0.5])
-    b = -0.2
-    
-    tmp = np.sum(w*x) + b
-    if tmp <= 0:
-        return 0
-    else:
-        return 1
-
-def XOR(x1, x2):
-    s1 = NAND(x1, x2)
-    s2 = OR(x1, x2)
-    y = AND(s1, s2)
-    return y
+import pickle
+import sys, os
+from mnist import load_mnist
 
 def step_function(x):
     y = x > 0
@@ -52,18 +16,56 @@ def relu(x):
 def identify_function(x):
     return x
 
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+# def init_network():
+#     network = {}
+#     network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+#     network['b1'] = np.array([0.1, 0.2, 0.3])
+#     network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+#     network['b2'] = np.array([[0.1, 0.2]])
+#     network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+#     network['b3'] = np.array([0.1, 0.2])
+
+#     return network
+
+# def forward(network, x):
+#     W1, W2, W3 = network['W1'], network['W2'], network['W3']
+#     b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+#     a1 = np.dot(x, W1) + b1
+#     z1 = sigmoid(a1)
+#     a2 = np.dot(z1, W2) + b2
+#     z2 = sigmoid(a2)
+#     a3 = np.dot(z2, W3) + b3
+#     y = identify_function(a3)
+
+#     return y
+
+# network = init_network()
+# x = np.array([1.0, 0.5])
+# y = forward(network, x)
+# print(y)
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
 def init_network():
-    network = {}
-    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-    network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-    network['b2'] = np.array([[0.1, 0.2]])
-    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
-    network['b3'] = np.array([0.1, 0.2])
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    weight_path = base_dir + "/sample_weight.pkl"
+    with open(weight_path, 'rb') as f:
+        network = pickle.load(f)
 
     return network
 
-def forward(network, x):
+def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
@@ -72,11 +74,18 @@ def forward(network, x):
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
-    y = identify_function(a3)
+    y = softmax(a3)
 
     return y
 
+x, t = get_data()
 network = init_network()
-x = np.array([1.0, 0.5])
-y = forward(network, x)
-print(y)
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
