75e17f15029c9686ce9af4cbd1ed325f9af9bbac
Kang Junhyeong <kangjj17@naver.com>
2026-01-07 23:55:54 +0900

    feat: 미니배치 학습 구현

--- python-dlfs1/Chap04/main.py
+++ python-dlfs1/Chap04/main.py
@@ -1,55 +1,32 @@
 import numpy as np
-import pickle
-import sys, os
 from mnist import load_mnist
-from functions import *
+from two_layer_net import TwoLayerNet
 
-def get_data():
-    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
-    return x_test, t_test
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-def init_network():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    weight_path = base_dir + "/sample_weight.pkl"
-    with open(weight_path, 'rb') as f:
-        network = pickle.load(f)
+train_loss_list = []
 
-    return network
+# 하이퍼파라미터
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
 
-def predict(network, x):
-    W1, W2, W3 = network['W1'], network['W2'], network['W3']
-    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = softmax(a3)
+for i in range(iters_num):
+    # 미니배치
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
 
-    return y
+    # 기울기 계산
+    grad = network.numerical_gradient(x_batch, t_batch)
 
-# x, t = get_data()
-# network = init_network()
-
-# batch_size = 100
-# accuracy_cnt = 0
-
-# for i in range(0, len(x), batch_size):
-#     x_batch = x[i:i+batch_size]
-#     y_batch = predict(network, x_batch)
-#     p = np.argmax(y_batch, axis=1)
-#     accuracy_cnt += np.sum(p == t[i:i+batch_size])
-
-# print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
-
-def gradient_descent(f, init_x, lr=0.01, step_num=100):
-    x = init_x
-
-    for i in range(step_num):
-        grad = numerical_gradient(f, x)
-        x -= lr * grad 
+    # 매개변수 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
     
-    return x
-
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
+    # 학습 경과 기록
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
